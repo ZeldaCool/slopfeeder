99e1da5f61b48b31aa538f51822a506889a65615
Person Boy <170485328+ZeldaCool@users.noreply.github.com>
2025-12-14 18:22:40 -0700

    basic page setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::response::Redirect;
+
+
+#[macro_use] extern crate rocket;
+
+
+#[get("/")]
+fn index() -> Redirect {
+    let mut sus_counter = 0;
+    if sus_counter >= 0{
+        Redirect::to(uri!("/feedertime"))
+    } else{
+        Redirect::to(uri!("/feedertime"))
+    }
 }
+#[get("/feedertime")]
+fn feeder_time() -> &'static str{
+    "Let's feed 'em slop :D"
+}
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, feeder_time])
+
+    }
